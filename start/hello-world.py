d5ec18704ed105777191023905947b3076e6e543
Joe-Cain <cain.joe@student.greenriver.edu>
2026-04-15 01:15:07 +0000

    Hello script change

---
+++ start/hello-world.py
@@ -0,0 +1,15 @@
+'''
+This is just a script to prompt user for username, then print hello and a positive affirmation
+'''
+
+#prompt the user for their name
+
+guest_name = input ("What is your name: ")
+
+#this is to say hello
+
+print ("hello, ", guest_name)
+
+#print positive affirmation
+
+print ("Today is going to be a great day. ")
