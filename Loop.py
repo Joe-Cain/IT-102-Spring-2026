2d70d9f1854936f313287af845c9f212f18edf93
Joe-Cain <cain.joe@student.greenriver.edu>
2026-04-23 20:30:23 +0000

    Loop run

---
+++ Loop.py
@@ -0,0 +1,11 @@
+'''Create a loop and a conditional that takes an input and if it's a yes, 
+yprint 10 times when a reply is given.
+'''
+
+#This is going to take an input from the user
+answer = input("Is today a good day? (Y/N)" ).lower()
+
+#if statement to check if the answer is y, if it is, print yeah it is
+if answer == "y":
+    for _ in range(10):
+        print("Yeah it is")
