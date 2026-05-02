34a7523bdcdcaff799f43b39c07408c1f1df4fe1
Joe-Cain <cain.joe@student.greenriver.edu>
2026-05-02 16:43:17 -0700

    Enhance ReadFile.py with author and file description
    Added author name and description to the script.

--- start/CH04/ReadFile.py
+++ start/CH04/ReadFile.py
@@ -1,3 +1,11 @@
 #!/usr/bin/env python3
 # Sample script that reads from a file
-# By 
+# By Joe Cain
+"""
+This is to read the file I created from the script writefile.py
+"""
+#create a loop to open file and read its contents
+with open("hackme.txt", "r") as file:
+    contents = file.read()
+    print("Here is someone to hack - info dump")
+    print(contents)
